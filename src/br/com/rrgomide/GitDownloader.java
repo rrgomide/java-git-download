32a651ac8829b7ba1402199150eaa2ee09d0b60b
rrgomide <raphael.gomide@gmail.com>
2017-07-13 22:24:19 -0300

    First commit

---
+++ src/br/com/rrgomide/GitDownloader.java
@@ -0,0 +1,7 @@
+package br.com.rrgomide;
+import java.util.List;
+
+public class GitDownloader {
+
+  private List<User> users;
+}
---
+++ src/br/com/rrgomide/User.java
@@ -0,0 +1,7 @@
+package br.com.rrgomide;
+
+public class User {
+
+  private String username;
+  
+}
